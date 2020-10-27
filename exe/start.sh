f000697920d1a15cc0c12a64f4c6c7f616814e06
岩瀬俊一郎 <iwaseshunichirou@iwaseshunichirounoMacBook-puro.local>
2020-10-27 10:27:45 +0900

    Change many files.

---
+++ exe/start.sh
@@ -0,0 +1,5 @@
+echo "call start.sh"
+
+java -Djava.net.preferIPv4Stack=true -Duser.timezone=Asia/Tokyo -Djava.util.logging.config.file=./logging.properties -jar ../target/apis-log-3.0.0-fat.jar -conf ./config.json
+
+echo "... done"

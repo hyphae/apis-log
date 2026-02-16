01d2b80fc71bbf6425ed55ef3976019244f95500
shweta verma <shwetaverma1404040@gmail.com>
2026-02-16 13:56:50 +0000

    fix: manually fix LogReceiver.java async methods
    Signed-off-by: shweta verma <shwetaverma1404040@gmail.com>

--- src/main/java/jp/co/sony/csl/dcoes/apis/tools/log/LogReceiver.java
+++ src/main/java/jp/co/sony/csl/dcoes/apis/tools/log/LogReceiver.java
@@ -122,9 +122,9 @@ public class LogReceiver extends AbstractVerticle {
 					if (printToStdout) System.out.println("[" + packet.sender() + "] " + String.valueOf(packet.data()).trim());
 				}).exceptionHandler(t -> {
 					log.error("exceptionHandler : " + t);
-				}).listen(port, listenAddress, resListen -> {
+				}).listen(port, listenAddress).onComplete(resListen -> {
 					if (resListen.succeeded()) {
-						socket.listenMulticastGroup(multicastGroupAddress, networkInterfaceName, null, resListenMulticastGroup -> {
+						socket.listenMulticastGroup(multicastGroupAddress, networkInterfaceName, null).onComplete(resListenMulticastGroup -> {
 							if (resListenMulticastGroup.succeeded()) {
 								if (log.isInfoEnabled()) log.info("log receive multicast service started on group address : " + multicastGroupAddress + ", port : " + port);
 								completionHandler.handle(Future.succeededFuture());

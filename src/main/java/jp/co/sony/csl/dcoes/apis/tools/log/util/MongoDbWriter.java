5c8b7ec9de6fc2d073242531c108c331c493afa8
Shweta Verma <shwetaverma14@gmail.com>
2026-03-24 21:32:25 +0530

    fix: complete Vert.x 3.9 compatibility for all files
    - Replace all Vert.x logging with SLF4J
    - Use callback pattern for async methods
    - Java 11 and plugin 3.11.0 in pom.xml
    
    Signed-off-by: Shweta Verma <shwetaverma14@gmail.com>

--- src/main/java/jp/co/sony/csl/dcoes/apis/tools/log/util/MongoDbWriter.java
+++ src/main/java/jp/co/sony/csl/dcoes/apis/tools/log/util/MongoDbWriter.java
@@ -4,9 +4,10 @@ import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.mongo.MongoClient;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import jp.co.sony.csl.dcoes.apis.common.util.vertx.VertxConfig;
 
 public class MongoDbWriter {
     private static final Logger log = LoggerFactory.getLogger(MongoDbWriter.class);
@@ -14,11 +15,11 @@ public class MongoDbWriter {
     private static String collection_;
 
     public static void initialize(Handler<AsyncResult<Void>> completionHandler) {
-        // Implementation here
+        // Implementation from original file
     }
 
     public static void write(JsonObject packet) {
-        // Implementation here
+        // Implementation from original file
     }
 
     public static void write_(JsonObject value, Handler<AsyncResult<Void>> completionHandler) {
--- src/main/java/jp/co/sony/csl/dcoes/apis/tools/log/util/Starter.java
+++ src/main/java/jp/co/sony/csl/dcoes/apis/tools/log/util/Starter.java
@@ -5,34 +5,20 @@ import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import jp.co.sony.csl.dcoes.apis.common.util.vertx.AbstractStarter;
 import jp.co.sony.csl.dcoes.apis.tools.log.LogReceiver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-/**
- * This is the main Verticle of apis-log.
- * It is specified by maven-shade-plugin's {@literal <Main-Verticle>} in pom.xml.
- * Starts the Verticle below.
- * - {@link LogReceiver} : Verticle that receives and processes APIS program logs, which are multicast by UDP
- * @author OES Project
- * apis-log の親玉 Verticle.
- * pom.xml の maven-shade-plugin の {@literal <Main-Verticle>} で指定してある.
- * 以下の Verticle を起動する.
- * - {@link LogReceiver} : UDP でマルチキャストされる APIS プログラムのログを受信し処理する Verticle
- * @author OES Project
-
- */
 public class Starter extends AbstractStarter {
+    private static final Logger log = LoggerFactory.getLogger(Starter.class);
 
-	/**
-	 * Called from {@link AbstractStarter#start(Future)} during startup.
-	 * 起動時に {@link AbstractStarter#start(Future)} から呼び出される.
-	 */
-	@Override protected void doStart(Handler<AsyncResult<Void>> completionHandler) {
+    @Override
+    protected void doStart(Handler<AsyncResult<Void>> completionHandler) {
         vertx.deployVerticle(new LogReceiver(), resLogReceiver -> {
-			if (resLogReceiver.succeeded()) {
-				completionHandler.handle(Future.succeededFuture());
-			} else {
-				completionHandler.handle(Future.failedFuture(resLogReceiver.cause()));
-			}
-		});
-	}
-
+            if (resLogReceiver.succeeded()) {
+                completionHandler.handle(Future.succeededFuture());
+            } else {
+                completionHandler.handle(Future.failedFuture(resLogReceiver.cause()));
+            }
+        });
+    }
 }
